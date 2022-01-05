15ca0ebe09430e4375e21a8bead37441b884be89
arvykwerti <95647218+arvykwerti@users.noreply.github.com>
2022-01-05 11:46:20 +0800

    brgynewsportal

---
+++ brgynewsportal/Brgy_News_portal/Brgy_News_portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brgy_News_portal.Startup))]
+namespace Brgy_News_portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
